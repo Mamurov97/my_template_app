02a24caeb633ec508e2baae2ef39132f7f78b835
golibjonmamurov <marshalbek2019@gmail.com>
2025-04-10 17:55:09 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:my_template_app/components/date_selects/single_month_picker_field.dart';
+import 'package:my_template_app/components/item_selects/multi_select_field.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// Asosiy ilova
+class City {
+  final int id;
+  final String name;
+
+  City(this.id, this.name);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Month Picker Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
+      title: 'MultiSelect Demo',
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MultiSelectExample(),
     );
   }
 }
 
-// Bosh sahifa
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MultiSelectExample extends StatefulWidget {
+  const MultiSelectExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MultiSelectExample> createState() => _MultiSelectExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  DateTime startFormatted = DateTime(2025, 01, 01);
-  DateTime endFormatted = DateTime(2025, 03, 30);
+class _MultiSelectExampleState extends State<MultiSelectExample> {
+  final List<City> cities = [
+    City(1, "Toshkent"),
+    City(2, "Samarqand"),
+    City(3, "Buxoro"),
+    City(4, "Xiva"),
+    City(5, "Farg‘ona"),
+    City(6, "Namangan"),
+    City(7, "Andijon"),
+    City(99, "Barchasi"), // Barchasini tanlash uchun maxsus ID
+  ];
+  List<City> selectedCities = [];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Month Picker Demo")),
+      appBar: AppBar(title: const Text("Shaharlar tanlash")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            SingleMonthPickerField(
-              labelText: "Tanlangan Oylar",
-              hintText: "Oylar oralig'ini tanlang",
-              startDate: DateTime(2020, 1),
-              endDate: DateTime.now(),
-              selectedMonth: startFormatted,
-              onSelectionChanged: (selectedRange) {
-                if (selectedRange != null) {
-                  setState(() {
-                    startFormatted = selectedRange;
-                  });
-                  debugPrint("Tanlangan sana: $startFormatted");
-                }
+            MultiSelectField<City>(
+              items: cities,
+              getName: (city) => city.name,
+              getId: (city) => city.id,
+              initialSelectedIds: [2, 3],
+              labelText: "Shaharlar",
+              hintText: "Shaharlarni tanlang",
+              selectAllWhenIdSelected: 99,
+              onSelectionChanged: (selected) {
+                setState(() {
+                  selectedCities = selected;
+                });
               },
             ),
-            const SizedBox(height: 20),
-            Text("Boshlanish: $startFormatted"),
           ],
         ),
       ),
